04757e8f504f3483925bb1e527234d3cdce43dd0
fogelkuba <fogelkuba@gmail.com>
2019-04-17 21:57:14 +0200

    fix

--- app.js
+++ app.js
@@ -5,14 +5,18 @@ const morgan = require('morgan');
 //port
 const PORT = 8080;
 
+
 //routes
 const postRoutes = require('./routes/post');
 
+
 //middleware
 app.use(morgan('dev'));
+
 
 app.use('/', postRoutes);
 
 app.listen(PORT, () => {
     console.log(`App is listening on port ${PORT}`);
 })
+
